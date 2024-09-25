31e578ff602580dfa8f1d52d751d093f9f10ffee
Oreon0529 <dhwnsgur4449@naver.com>
2024-09-25 10:47:37 +0900

    회원가입 및 로그인

---
+++ app/src/main/java/com/example/aerospace/RegisterActivity.kt
@@ -0,0 +1,30 @@
+package com.example.aerospace
+
+import android.os.Bundle
+import android.webkit.WebView.FindListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var adapter: ArrayAdapter<CharSequence>
+    private lateinit var spinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        spinner = findViewById(R.id.majorSpinner)
+        adapter = ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+}
