176f1ef08e9e6e5e43b3683b5db52305302d7ccb
woals <woalsrn@Naver.com>
2024-11-25 22:15:12 +0900

    흐음

--- app/src/main/java/com/example/aerospace/BuildingMapActivity.kt
+++ app/src/main/java/com/example/aerospace/BuildingMapActivity.kt
@@ -2,7 +2,6 @@ package com.example.aerospace
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.ImageView
 import androidx.viewpager.widget.ViewPager
 
 class BuildingMapActivity : AppCompatActivity() {
@@ -12,13 +11,18 @@ class BuildingMapActivity : AppCompatActivity() {
         setContentView(R.layout.activity_building_map)
 
         val viewPager: ViewPager = findViewById(R.id.viewPager)
+
+        // 이미지 리스트 (순서 조정 포함)
         val imageList = listOf(
             R.drawable.firstfloor,
             R.drawable.secondfloor,
+            R.drawable.fivefloor, // fivefloor 추가
             R.drawable.sixfloor,
             R.drawable.sevenfloor,
             R.drawable.eightfloor
         )
+
+        // 어댑터에 리스트 전달
         val adapter = ImagePagerAdapter(this, imageList)
         viewPager.adapter = adapter
     }
